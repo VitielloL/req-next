cddc6f8c4f0080ea453ab7cdefb6fec4645f84ad
Lucas Vitiello <lucasvitielloo@gmail.com>
2024-07-10 23:04:00 -0300

    add tipagem page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,41 +2,47 @@
 
 import { useState, useEffect } from "react";
 
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 export default function Home() {
-
-  const URL_API = 'https://jsonplaceholder.typicode.com/todos/'
+  const URL_API = 'https://jsonplaceholder.typicode.com/todos/';
   // const URL_API = 'https://jsonplaceholder.typicode.com/todos/1'
-  const [data,setData] = useState(null)
-
+  const [data, setData] = useState<Todo[] | null>(null);
   const [loading, setLoading] = useState(false);
+
   const fetchAllData = async () => {
     try {
       setLoading(true);
       const response = await fetch(URL_API);
-      const data = await response.json();
+      const data: Todo[] = await response.json();
 
-      if (!data) 
-        throw 'Problema na Requisição' 
-        setData(data);
+      if (!data) {
+        throw new Error('Problema na Requisição');
+      }
+      setData(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     fetchAllData();
-  },[])
+  }, []);
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      {loading && !data &&
-        <p>Carregando Informações...</p>
-      }
+      {loading && !data && <p>Carregando Informações...</p>}
 
       {data && data.map((item) => (
-        <p key={item.id}>{item.title}</p>))}
+        <p key={item.id}>{item.title}</p>
+      ))}
     </div>
   );
 }
